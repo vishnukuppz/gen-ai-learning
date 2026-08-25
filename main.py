8451deab6024775731a0c7df341b6b5c33c5d867
Kuppusamy <kuppusamy.m@keka.com>
2026-08-25 23:14:23 +0530

    Grade detector - Assignment Day 2

---
+++ main.py
@@ -0,0 +1,18 @@
+
+mark = int(input("Enter your mark: "))
+
+#raise exception if mark is less than 0 or greater than 100
+if(mark < 0 or mark > 100):
+    raise ValueError("Invalid mark. Please enter a mark between 0 and 100.")
+
+
+if mark >= 90:
+    print("Grade: A")
+elif mark >= 80:
+    print("Grade: B")
+elif mark >= 70:
+    print("Grade: C")
+elif mark >= 60:    
+    print("Grade: D")
+else:
+    print("Grade: E")
